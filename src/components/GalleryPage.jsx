7c9d9351dd47539af10d1e56c397b336d939bb2d
MatejMihailovic <mihailovicmatej@gmail.com>
2025-01-16 23:56:39 +0100

    Added images

--- src/components/GalleryPage.jsx
+++ src/components/GalleryPage.jsx
@@ -12,6 +12,13 @@ import img7 from "../assets/img/6.jpg";
 import img8 from "../assets/img/7.jpg";
 import img9 from "../assets/img/8.jpg";
 import img10 from "../assets/img/9.jpg";
+import img11 from "../assets/img/11.jpg";
+import img12 from "../assets/img/12.jpg";
+import img13 from "../assets/img/13.jpg";
+import img14 from "../assets/img/14.jpg";
+import img15 from "../assets/img/15.jpg";
+import img16 from "../assets/img/16.jpg";
+import img17 from "../assets/img/17.jpg";
 
 //Imports for images
 const images = [
@@ -24,7 +31,14 @@ const images = [
   img7,
   img8,
   img9,
-  img10
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
 ]
 
 export default function GalleryPage({ language }) {
